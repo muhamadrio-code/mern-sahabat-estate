3d7fec16028445d7de17717e9eefeadea6ff7283
muhammad rio <muhammad.rioperm@gmail.com>
2023-10-19 21:31:09 +0700

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_DB)
+  .then(() => {
+    console.log("Connect to database");
+  })
+  .catch((e) => {
+    console.error(e);
+  })
 
 const app = express()
 app.listen(3030, () => {
-  
+
   console.log("Server running on port 3030..");
 })
