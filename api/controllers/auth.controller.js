ef5e726f235e3776bb88de215ab39c47eaf218dc
muhammad rio <muhammad.rioperm@gmail.com>
2023-10-20 16:51:27 +0700

    add password hashes before save it to the database

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,8 +2,9 @@ import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 const signupController = async (req, res, next) => {
   const { username, email, password } = req.body
+  const hashedPassword = bcryptjs.hashSync(password, 14)
   const newUser = new User(
-    { username, email, password }
+    { username, email, password: hashedPassword }
   )
   await newUser.save()
     .then(() => {
