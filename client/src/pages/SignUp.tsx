4f4ffa16326f48a2ffdc9d807e6cd38cc73f6c20
muhammad rio <muhammad.rioperm@gmail.com>
2023-10-21 19:40:41 +0700

    use From as it  root instead of div

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,6 +1,5 @@
 import { Form, useActionData, useNavigation } from "react-router-dom";
 import { MyLink } from "../components";
-import { ComponentProps, FC, useMemo } from "react";
 
 const Spinner = () => {
   return <div
@@ -12,37 +11,36 @@ const Spinner = () => {
   </div>
 }
 
-const SignUpButton: FC<ComponentProps<'div'>> | string = () => {
+const SignUpButton = () => {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting"
   const buttonColor = isSubmitting ? "bg-primary-50" : "bg-primary"
   const cursor = isSubmitting ? "cursor-wait" : "cursor-pointer"
 
-  return <button disabled={isSubmitting} type="submit"
-    className={`${buttonColor} ${cursor} hover:bg-primary-50 flex justify-center items-center w-full h-12 py-3 rounded-lg text-white font-semibold`}
-  >
-    {isSubmitting ? <Spinner /> : "Sign Up"}
-  </button>
+  return (
+    <button disabled={isSubmitting} type="submit"
+      className={`${buttonColor} ${cursor} hover:bg-primary-50 flex justify-center items-center w-full h-12 py-3 rounded-lg text-white font-semibold`}>
+      {isSubmitting ? <Spinner /> : "Sign Up"}
+    </button>
+  )
 }
 
 export default function SignUp() {
   const error: Error | undefined = useActionData() as Error
-  const message = useMemo(() => error?.message,[error])
-
   const inputClassStyle = "p-3 rounded-lg w-full"
   return (
-    <div className="sm:w-[32rem] mt-4 p-4 m-auto flex-col min-h-min flex justify-center items-center gap-6">
-      <h1 className="text-2xl font-bold p-4">Sign Up</h1>
-      <Form method="post" className="w-full m-auto flex-col min-h-min flex justify-center items-center gap-5">
+    <Form method="post" className="sm:w-[32rem] mt-4 p-4 m-auto flex-col min-h-min flex justify-center items-center gap-6" replace>
+      <div className="w-full m-auto flex-col min-h-min flex justify-center items-center gap-5">
+        <h1 className="text-2xl font-bold p-4">Sign Up</h1>
         <input id='username' type="text" name="username" placeholder="username" className={inputClassStyle} required />
         <input id='email' type="email" name="email" placeholder="email" className={inputClassStyle} required />
         <input id='password' type="password" name='password' placeholder="Password" className={inputClassStyle} required minLength={6} />
         <SignUpButton />
-      </Form>
-      <p className="text-red-700 tex-sm italic">{error && message}</p>
-      <h4 className="mr-auto">
-        Have an account? <MyLink relative="path" to='../signin' className="text-secondary">Sign in</MyLink>
-      </h4>
-    </div>
+        <p className="text-red-700 tex-sm italic">{error && error?.message}</p>
+        <h4 className="mr-auto">
+          Have an account? <MyLink relative="path" to='../signin' className="text-secondary">Sign in</MyLink>
+        </h4>
+      </div>
+    </Form>
   )
 }
