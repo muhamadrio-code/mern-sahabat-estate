0fe92e2c176bf3369223cc993e102f2d7e6a2e6e
muhammad rio <muhammad.rioperm@gmail.com>
2023-10-22 12:57:17 +0700

    send a valid user as a response data

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -29,11 +29,13 @@ const signinController = async (req, res, next) => {
     const validPassword = bcryptjs.compareSync(password, validUser.password) 
     if(!validPassword) return next(errorHandler(401, "Wrong Credientials!"))
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+    const { password: pass, ...rest } = validUser._doc
     res
     .cookie('access_token', token, { httpOnly: true, expires: new Date(Date.now() + (24 * 60 * 60 * 1000)) })
     .status(200)
     .json({
-      success: true
+      success: true,
+      data: rest
     })
   } catch (error) {
     next(error)
