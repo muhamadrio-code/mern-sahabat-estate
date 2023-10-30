80e7e8657b155f76df0a3c4ecf122c6f1102f48b
muhammad rio <muhammad.rioperm@gmail.com>
2023-10-30 14:54:17 +0700

    change logo

--- client/src/components/CompanyLogo.tsx
+++ client/src/components/CompanyLogo.tsx
@@ -4,7 +4,7 @@ export function CompanyLogo() {
   return (
     <Link className="cursor-pointer" to='/'>
       <h1 className='text-xl font-bold text-slate-500 text-center'>
-        Sahand
+        Sahabat
         <span className='text-slate-700'>Estate</span>
       </h1>
     </Link>
